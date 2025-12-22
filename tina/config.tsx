41a26bb60c0d0d2e51bc01fbad2ca033ad65cde1
julesdo <julescamilledore@gmail.com>
2025-12-22 19:53:59 +0100

    maj 2.12 trad prod

--- tina/config.tsx
+++ tina/config.tsx
@@ -11,12 +11,12 @@ import Team from "./collection/team";
 import Tag from "./collection/tag";
 
 const config = defineConfig({
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || '',
   branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH! || // custom branch env override
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF! || // Vercel branch env
-    process.env.HEAD!, // Netlify branch env
-  token: process.env.TINA_TOKEN!,
+    process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
+    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
+    process.env.HEAD || 'main', // Netlify branch env
+  token: process.env.TINA_TOKEN || '',
   media: {
     // If you wanted cloudinary do this
     // loadCustomStore: async () => {
